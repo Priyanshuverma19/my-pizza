a5fb8c53f4b49233ec203106de93ff8ff7eaaaf0
priyanshu verma <priyanshubalram19112001@gmail.com>
2024-01-26 02:27:40 +0530

    value error

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -72,7 +72,7 @@ export async function POST(req) {
         shipping_rate_data: {
           display_name: 'Delivery fee',
           type: 'fixed_amount',
-          fixed_amount: {amount: 5000, currency: 'INR'},
+          fixed_amount: {amount: 500, currency: 'INR'},
         },
         
       }
